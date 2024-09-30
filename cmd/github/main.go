6dfd67984a927bc20c47793b6488ee3b1a2f1b0f
魏攀6092004007 <wei.pan2@zte.com.cn>
2024-09-30 13:44:33 +0800

    Up

--- cmd/github/main.go
+++ cmd/github/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/Chara-X/github"
 )
 
 func main() {
-	var reg = github.Registry{}
+	var reg = github.Registry{Exclude: os.Args[4:]}
 	reg.Path, _ = os.Getwd()
+	fmt.Println(reg.Path)
 	switch os.Args[1] {
 	case "push":
 		reg.Push(os.Args[2], os.Args[3])
