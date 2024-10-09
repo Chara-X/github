bda1c2c5243159ed0eba35817d7bd9ef2f4dd5c8
魏攀6092004007 <wei.pan2@zte.com.cn>
2024-10-09 18:11:02 +0800

    Up

--- Registry.go
+++ Registry.go
@@ -17,21 +17,21 @@ type Registry struct {
 }
 
 func (r *Registry) Push(registry, branch string) {
-	var wg = sync.WaitGroup{}
+	// var wg = sync.WaitGroup{}
 	filepath.Walk(r.Path, func(path string, info fs.FileInfo, err error) error {
 		if isRepo(path) && !slices.Contains(r.Ignore, info.Name()) {
-			wg.Add(1)
-			go func() {
-				var cmd = exec.Command("sh", "-c", fmt.Sprintln("git add --all; git commit --message Up; git push", registry+"/"+info.Name()+".git", "HEAD:"+branch))
-				cmd.Dir = path
-				var out, _ = cmd.Output()
-				log.Println(path + "\n" + cmd.String() + "\n" + string(out))
-				wg.Done()
-			}()
+			// wg.Add(1)
+			// go func() {
+			var cmd = exec.Command("sh", "-c", fmt.Sprintln("git add --all; git commit --message Up; git push", registry+"/"+info.Name()+".git", "HEAD:"+branch))
+			cmd.Dir = path
+			var out, _ = cmd.Output()
+			log.Println(path + "\n" + cmd.String() + "\n" + string(out))
+			// wg.Done()
+			// }()
 		}
 		return nil
 	})
-	wg.Wait()
+	// wg.Wait()
 }
 func (r *Registry) Pull(registry, branch string) {
 	var wg = sync.WaitGroup{}
